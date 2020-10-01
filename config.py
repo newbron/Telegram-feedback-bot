3af0415c60adab84d213a844811e4a7cd5c0e7a8
newbron <62109375+newbron@users.noreply.github.com>
2020-10-01 14:05:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,9 @@ from pathlib import Path
 
 from command_replies import Replies
 
-TG_TOKEN = "YOUR TOKEN HERE"
+TG_TOKEN = "1218909303:AAE606UBWw_8lGHVKH0WYhvwiMNvXJjhJWQ"
 PROXY = None
-CHAT_ID = -100125 # The chat group or individual chat id of forwarded message destination
+CHAT_ID = -1001338488105 # The chat group or individual chat id of forwarded message destination
 BOT_TIMEOUT = 10
 BOT_RETRIES = 5
 USER_JSON = 'user_book.json'
